5b85b8590d1ab3de5d97162b975e8d04ae929293
ELeonJohnson <enochjohnsonleon@gmail.com>
2018-05-02 21:56:53 -0400

    Added buttons and menu

--- dist/script.067f7ddc.js
+++ dist/script.067f7ddc.js
@@ -148,7 +148,7 @@ module.exports = reloadCSS;
 'use strict';
 
 require('../scss/main.scss');
-},{"../scss/main.scss":5}],17:[function(require,module,exports) {
+},{"../scss/main.scss":5}],20:[function(require,module,exports) {
 
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -317,5 +317,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},[17,3])
+},{}]},{},[20,3])
 //# sourceMappingURL=/script.067f7ddc.map
