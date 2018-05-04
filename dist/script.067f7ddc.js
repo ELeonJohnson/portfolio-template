dcf4fb1d6a0dd6e8ef644a88152217478e0e5b19
ELeonJohnson <enochjohnsonleon@gmail.com>
2018-05-04 02:56:36 -0400

    Worked on portfolio page (html css)

--- dist/script.067f7ddc.js
+++ dist/script.067f7ddc.js
@@ -77,7 +77,7 @@ parcelRequire = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({7:[function(require,module,exports) {
+})({8:[function(require,module,exports) {
 var bundleURL = null;
 function getBundleURLCached() {
   if (!bundleURL) {
@@ -107,7 +107,7 @@ function getBaseURL(url) {
 
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
-},{}],6:[function(require,module,exports) {
+},{}],7:[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -138,17 +138,17 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":7}],3:[function(require,module,exports) {
+},{"./bundle-url":8}],4:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":6}],5:[function(require,module,exports) {
+},{"_css_loader":7}],6:[function(require,module,exports) {
 'use strict';
 
 require('../scss/main.scss');
-},{"../scss/main.scss":3}],8:[function(require,module,exports) {
+},{"../scss/main.scss":4}],25:[function(require,module,exports) {
 
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -178,7 +178,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '49908' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '50888' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -317,5 +317,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},[8,5])
+},{}]},{},[25,6])
 //# sourceMappingURL=/script.067f7ddc.map
